890bf5c375b358916c7266fe9a18ca77651e8c22
alaricXshiran <alaric.shir4n@gmail.com>
2024-11-22 15:10:06 +0530

    edit port 1

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const express = require("express");
 const app = express();
 
 //security middleware
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again later.",
+});
+
+app.use(limiter); // Apply rate limiting middleware
 app.use((req, res, next) => {
   const apiKey = req.headers["api-key"];
   if (apiKey === process.env.API_KEY) {
